13c6142f0c7e11b006b9d5efe66de66459e1b0d7
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-22 17:05:02 +0600

    shared left and right nav bar added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,15 +2,40 @@ import React from 'react';
 import Header from '../pages/Shared/Header/Header';
 import Footer from '../pages/Shared/Footer/Footer';
 import { Outlet } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
+import LeftNav from '../pages/Shared/LeftNav/LeftNav';
+import RightNav from '../pages/Shared/RightNav/RightNav';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-            <Outlet> </Outlet>
+                <Container>
+                    <Row>
+
+                        <Col sm={3}>
+                          <LeftNav> </LeftNav>
+                        </Col>
+
+                        <Col sm={6}>
+                          <h2>Main Container Coming</h2>
+                        </Col>
+
+                        <Col sm={3}>
+                          <RightNav> </RightNav>
+                        </Col>
+
+                    </Row>
+                    <Row>
+                        <Col sm>sm=true</Col>
+                        <Col sm>sm=true</Col>
+                        <Col sm>sm=true</Col>
+                    </Row>
+            </Container>
             <Footer> </Footer>
         </div>
     );
 };
 
 export default Main;
+
---
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightNav = () => {
+    return (
+        <div>
+            <h2>This is Right Nav</h2>
+        </div>
+    );
+};
+
+export default RightNav;
