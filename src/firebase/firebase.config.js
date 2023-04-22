4d0d66233708a71c8468c84a45ab2d3156e945f9
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-22 13:07:52 +0600

    firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAm2e1JfZnwN14pauG977CNoRranyKTKsk",
+  authDomain: "the-usa-news-280d2.firebaseapp.com",
+  projectId: "the-usa-news-280d2",
+  storageBucket: "the-usa-news-280d2.appspot.com",
+  messagingSenderId: "895335806915",
+  appId: "1:895335806915:web:fd9affd1809e94f0c16f76"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
