0776b59ef56faf48292f851a163bc252fc9eca0d
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-22 21:58:20 +0600

    catagori data render complete

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const LeftNav = () => {
     const [catagories, setCatagories] = useState([]);
@@ -8,10 +9,19 @@ const LeftNav = () => {
         .then( data => setCatagories(data))
     } ,[])
 
-    console.log(catagories);
     return (
         <div>
-            <h2>This is left Navbar</h2>
+            <h4 className="fw-semibold">All Caterogy</h4>
+
+            <div className="ps-4 py-3 my-2">
+                {
+                    catagories.map( catagory => <p 
+                        key={catagory.id}> 
+                        <NavLink to={`/catagory/${catagory.id}`} className='text-decoration-none text-secondary fw-semibold'  >{catagory?.name}</NavLink>
+
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
