3a8beff0c463f9e21286235c3234f1c9ea3fc47b
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-22 17:40:22 +0600

    date time added using moment js

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Logo from '../../../assets/logo.png';
-
+import moment from 'moment';
 const Header = () => {
     return (
         <div>
-            <img src={Logo} alt="" />
+            <div className="text-center ">
+            <img className='img-fluid' src={Logo} alt="" />
             <p className='text-secondary'>Journalism Without Fear or Favour<small></small></p>
+            <p>{moment().format("dddd")}, <span className='text-secondary'>{moment().format("MMMM D, YYYY")}</span></p>
+            </div>
         </div>
     );
 };
