65a56f13066b8400382dc17c8953237d86920406
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-22 14:02:29 +0600

    project stracture added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}> </RouterProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+           <p className='text-center'><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, quas!</small></p>
+        </div>
+    );
+};
+
+export default Footer;
