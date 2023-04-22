9eb9fe0cc3590f6f65549eeea64b6b33ad57d0d6
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-22 18:53:49 +0600

    login button added

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import { Button } from 'react-bootstrap';
+import { BsGoogle, BsGithub } from 'react-icons/bs';
 const RightNav = () => {
     return (
         <div>
-            <h2>This is Right Nav</h2>
+            <h4 className='fw-semibold'>Login With</h4>
+            <Button className='w-100 mb-2' variant="outline-primary"> <BsGoogle/> Login with Google</Button>
+            <br />
+            <Button className='w-100' variant="outline-dark"><BsGithub/> Login with Github</Button>
         </div>
     );
 };
