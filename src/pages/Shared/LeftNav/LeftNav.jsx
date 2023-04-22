25a78cc921c816b0bf476fb736b546e449d68a37
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-22 21:39:45 +0600

    catagoris data loading complete

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeftNav = () => {
+    const [catagories, setCatagories] = useState([]);
+    useEffect( () => {
+        fetch('http://localhost:5000/catagories')
+        .then( res => res.json())
+        .then( data => setCatagories(data))
+    } ,[])
+
+    console.log(catagories);
     return (
         <div>
             <h2>This is left Navbar</h2>
