85ce57f74bd0b0b9fa3d1f47ee287813c7ae0285
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-23 22:44:45 +0600

    date fomation and share, bookmark icons added

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,6 +1,8 @@
+import moment from "moment/moment";
 import React from "react";
 import {  Card, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import {BsBookmark, BsShare} from "react-icons/bs";
 
 const NewsCard = ({ news }) => {
   const {
@@ -16,8 +18,16 @@ const NewsCard = ({ news }) => {
   } = news;
   return (
       <Card className=" mb-4">
-        <Card.Header>
-          <Image src={author?.img} roundedCircle /> 
+        <Card.Header className="d-flex align-items-center">
+          <Image style={{widows:'40px', height:'40px'}} src={author?.img} roundedCircle /> 
+          <div className="ps-2 flex-grow-1">
+            <p className="mb-0">{author?.name}</p>
+            <p><small>{moment(author?.published_date).format('YYYY-MM-D')}</small></p>
+          </div>
+          <div>
+            <BsBookmark className="me-2"/>
+            <BsShare className="ms-1" />
+          </div>
         </Card.Header>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
