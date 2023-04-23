7fa8b53ef496172019f9e2926a4cae953d84e38e
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-23 20:17:47 +0600

    new card data added

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import {  Card } from "react-bootstrap";
 
-const NewsCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const NewsCard = ({ news }) => {
+  const {
+    _id,
+    others_info,
+    category_id,
+    rating,
+    total_view,
+    title,
+    author,
+    image_url,
+    details,
+  } = news;
+  return (
+      <Card className=" mb-4">
+        <Card.Header>Featured</Card.Header>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Img variant="top" src={image_url} /> 
+          <Card.Text>
+            {details}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      </Card>
+  );
 };
 
 export default NewsCard;
