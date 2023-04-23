09409995faac066c6db418dc09bf2abf956398df
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-23 22:25:30 +0600

    news details optional rendering added and author image added

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {  Card } from "react-bootstrap";
+import {  Card, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const NewsCard = ({ news }) => {
   const {
@@ -15,12 +16,14 @@ const NewsCard = ({ news }) => {
   } = news;
   return (
       <Card className=" mb-4">
-        <Card.Header>Featured</Card.Header>
+        <Card.Header>
+          <Image src={author?.img} roundedCircle /> 
+        </Card.Header>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Img variant="top" src={image_url} /> 
           <Card.Text>
-            {details}
+            {details.length < 250 ? <>{details}</> : <> {details.slice(0, 250)} ... <Link to ={`/news/${_id}`} >Read More</Link> </>}
           </Card.Text>
         </Card.Body>
         <Card.Footer className="text-muted">2 days ago</Card.Footer>
