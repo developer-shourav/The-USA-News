4ff8fa056c435cc6bf73bfff8239aa1725228c19
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-23 23:39:06 +0600

    news card added

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { Link, useLoaderData } from 'react-router-dom'; 
+import {HiArrowLeft} from 'react-icons/hi';
 
 const News = () => {
+    const news = useLoaderData() ;
+    const {
+        _id,
+        others_info,
+        category_id,
+        rating,
+        total_view,
+        title,
+        author,
+        image_url,
+        details,
+      } = news;
     return (
-        <div>
-            <h2>News Details Coming Soon</h2>
-        </div>
+        <Card >
+        <Card.Img className='p-3' variant="top" src={image_url} />
+        <Card.Body>
+            <Card.Title style= {{color:"#403F3F"}} className=' fw-semibold'>{title}</Card.Title>
+            <Card.Text style= {{color:"#706F6F"}}>
+              {details}
+            </Card.Text>
+            <Link to='/category/0' className='text-decoration-none'> <Button style={{backgroundColor:'#D72050'}} className='rounded-0 border-0 px-4 d-flex align-items-center gap-1' variant="primary"> <HiArrowLeft/> <span>All news in this category </span></Button></Link>
+        </Card.Body>
+     </Card>
     );
 };
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -31,7 +31,8 @@ const router = createBrowserRouter([
         children: [
             {
                 path:':id',
-                element:<News> </News>
+                element:<News> </News>,
+                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
             }
         ]
     }
