775e3de310dbeb73d164ef2d24e4203c53d71ee8
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-23 07:55:55 +0600

    news layout and component added , route setup complete

---
+++ src/pages/News/News.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const News = () => {
+    return (
+        <div>
+            <h2>News Details Coming Soon</h2>
+        </div>
+    );
+};
+
+export default News;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
 import Category from "../pages/Home/Category/Category";
+import NewsLayout from "../layouts/NewsLayout";
+import News from "../pages/News/News";
 
 const router = createBrowserRouter([
     {
@@ -21,6 +23,17 @@ const router = createBrowserRouter([
             },
         ]
     },
+
+    {
+        path: '/news',
+        element:<NewsLayout> </NewsLayout>,
+        children: [
+            {
+                path:':id',
+                element:<News> </News>
+            }
+        ]
+    }
 ])
 
 
