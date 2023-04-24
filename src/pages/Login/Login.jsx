2ed669b1154f1e77d1ba323c69531cb4e370892a
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-24 14:45:22 +0600

    regitser added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
@@ -8,23 +9,33 @@ const Login = () => {
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" name="email" placeholder="Enter email" required />
           
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" name="password" placeholder="Password" required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
         <Button variant="primary" type="submit">
-          Submit
+          Login
         </Button>
-        <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+        <br />
+        <Form.Text className="text-secondary">
+          
+           Don't Have an Account? <Link to='/register'>Register</Link>
+        </Form.Text>
+        <Form.Text className="text-success">
+            
+        </Form.Text>
+
+        <Form.Text className="text-danger">
+           
+        </Form.Text>
+
       </Form>
     </Container>
   );
